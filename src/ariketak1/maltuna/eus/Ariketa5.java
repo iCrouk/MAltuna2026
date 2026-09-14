d446051035af815a2be1ccfe3cd61d24904cc258
iCrouk <ikercoranti@gmail.com>
2026-09-14 11:14:44 +0200

    [ADD] Se añaden ejercicio 4,5 y 6 sin código.

---
+++ src/ariketak1/maltuna/eus/Ariketa5.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ariketak1.maltuna.eus;
+
+/**
+ * 
+ */
+public class Ariketa5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ariketak1/maltuna/eus/Ariketa6.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ariketak1.maltuna.eus;
+
+/**
+ * 
+ */
+public class Ariketa6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
