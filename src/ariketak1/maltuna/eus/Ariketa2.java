34eb7755f7188add4d31b95eef468c1b58925590
iCrouk <ikercoranti@gmail.com>
2026-09-14 10:43:46 +0200

    [MOD] Se ha añadido codigo nuevo a Ariketa2.java

--- src/ariketak1/maltuna/eus/Ariketa2.java
+++ src/ariketak1/maltuna/eus/Ariketa2.java
@@ -2,6 +2,7 @@
  * 
  */
 package ariketak1.maltuna.eus;
+import java.util.*;
 
 /**
  * 
@@ -13,7 +14,21 @@ public class Ariketa2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int znb1,znb2;
+		
+		System.out.print("Idatzi lehen zenbakia, mesedez");
+		znb1 =sc.nextInt();
+		
+		System.out.print("Idatzi bigarren zenbakia, mesedez");
+		znb2 =sc.nextInt();
+		
+		System.out.print("Idatzi lehen zenbakia: "+znb1);
+		System.out.print("Idatzi bigarren zenbakia: "+znb2);
+		
+		sc.close();
 	}
 
 }
