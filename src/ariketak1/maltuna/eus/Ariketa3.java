821714f6c487b81c6679b1e0c43c45d206eae071
iCrouk <ikercoranti@gmail.com>
2026-09-14 10:44:10 +0200

    [ADD] Se ha generado nueva Clase Ariketa3

---
+++ src/ariketak1/maltuna/eus/Ariketa3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ariketak1.maltuna.eus;
+
+/**
+ * 
+ */
+public class Ariketa3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
