86f88cde328d4c632c5adfbaa49fa575d1797ec9
icoranti <icoranti@192.168.1.148>
2026-09-14 20:05:05 +0200

    [UPD] add code to Ariketa5.java

--- src/ariketak1/maltuna/eus/Ariketa5.java
+++ src/ariketak1/maltuna/eus/Ariketa5.java
@@ -3,8 +3,11 @@
  */
 package ariketak1.maltuna.eus;
 
+import java.util.Scanner;
+
 /**
- * 
+ * Bi zenbaki oso eskatuko dituzu. Bakoitza aldagai batean gordeko duzu.
+Zenbaki handiena bistaratuko duzu.
  */
 public class Ariketa5 {
 
@@ -13,7 +16,21 @@ public class Ariketa5 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner nireSc = new Scanner(System.in);
+		int znb1,znb2;
+		
+		znb1= nireSc.nextInt();
+		znb2= nireSc.nextInt();
+		
+		int handiena;
+		
+		if (znb1>znb2) {
+			handiena = znb1;
+		}else {
+			handiena =znb2;
+		}
+	
+		System.out.println("Bi zenbakien artean:"+handiena+" da handiena");
 	}
 
 }
