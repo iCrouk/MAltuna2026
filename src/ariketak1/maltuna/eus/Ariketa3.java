81d71013453f094f7664a0c1d5028a455d620e8f
icoranti <icoranti@192.168.1.148>
2026-09-14 19:43:47 +0200

    [UPD] Add code to Ariketa3.java. Add code to Ariketa4.java

--- src/ariketak1/maltuna/eus/Ariketa3.java
+++ src/ariketak1/maltuna/eus/Ariketa3.java
@@ -3,6 +3,8 @@
  */
 package ariketak1.maltuna.eus;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,6 +15,23 @@ public class Ariketa3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	Scanner nireSc = new Scanner(System.in);
+		
+		int znb1,znb2;
+		
+		System.out.print("Idatzi lehen zenbakia, mesedez");
+		znb1 =nireSc.nextInt();
+		
+		System.out.print("Idatzi bigarren zenbakia, mesedez");
+		znb2 =nireSc.nextInt();
+		
+		System.out.println(znb1+" eta "+znb2+"-ren arteko batuketa: "+ znb1 + znb2+" da");
+		System.out.println(znb1+" eta "+znb2+"-ren arteko kenketa: "+ znb1 +" da");
+		System.out.println(znb1+" eta "+znb2+"-ren arteko biderketa: "+ znb1 * znb2+" da");
+		System.out.println(znb1+" eta "+znb2+"-ren arteko zatiketa: "+ znb1 / znb2+" da");
+		
+		nireSc.close();
 
 	}
 
--- src/ariketak1/maltuna/eus/Ariketa4.java
+++ src/ariketak1/maltuna/eus/Ariketa4.java
@@ -2,9 +2,13 @@
  * 
  */
 package ariketak1.maltuna.eus;
+import java.util.*;
 
 /**
- * 
+ * Produktu baten salneurria eskatuko duzu (hamartarrak sartzeko aukera
+izan behar duzu); azken prezioa kalkulatuko duzu, kontuan izanda,
+produktuak %15eko deskontua daukala. (Oharra: %15eko deskontua,
+konstante bati esleitu).
  */
 public class Ariketa4 {
 
@@ -13,7 +17,15 @@ public class Ariketa4 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner nireSc = new Scanner(System.in);
+		int balioa;
+		final int deskontua = 15;
+		
+		System.out.println("Idatzi produktuaren salneurria");
+		balioa =nireSc.nextInt();
+		System.out.println("Deskontu gabeko salneurria:"+balioa+" deskontua:"+deskontua+" Salneurri berria:"+balioa * (1-deskontua));
+		nireSc.close();
 	}
 
 }
