2012c5e3840fffb749b047d0d86b7403ee9e5bc9
icoranti <icoranti@HZ397172.maltuna.local>
2026-09-15 08:27:12 +0200

    [UPD] Add new code with errors to Ariketak3 and Ariketak 5. Problems importing java.util.* object. Review needed.

--- src/ariketak1/maltuna/eus/Ariketa5.java
+++ src/ariketak1/maltuna/eus/Ariketa5.java
@@ -1,11 +1,5 @@
-/**
- * 
- */
 package ariketak1.maltuna.eus;
 
-/**
- * 
- */
 public class Ariketa5 {
 
 	/**
@@ -13,7 +7,28 @@ public class Ariketa5 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//INPUT
+		int znb1;
+		int znb2;
+		int handiena;
+		//EGITEKOAK
+		Scanner sc = new Scanner(System.in);
+		//lehen zenbakia eskatu eta gorde
+		System.out.println("Idatzi lehen zenbakia, mesedez");
+		znb1 = sc.nextInt();
+		//bigarren zenbakia eskatu eta gorde
+		System.out.println("Idatzi bigarren zenbakia, mesedez");
+		znb2 = sc.nextInt();
+		
+		if( znb1 < znb2) {
+			//System.out.println ("handiena: "+znb2+" da");
+			handiena = znb2;
+		} else {
+			//System.out.println ("handiena: "+znb1+" da");
+			handiena = znb1;
+		}
+		//OUTPUT
+		System.out.println ("handiena: "+handiena+" da");
 	}
 
 }
