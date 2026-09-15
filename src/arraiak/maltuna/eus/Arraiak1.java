43747be5af27d9239244ff58ac043fae34884ca9
iCrouk <ikercoranti@gmail.com>
2026-09-15 19:42:51 +0200

    [ADD] Arraiak1.java first commit, no code, only declare

---
+++ src/arraiak/maltuna/eus/Arraiak1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package arraiak.maltuna.eus;
+
+/**
+ * 
+ */
+public class Arraiak1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nireArraia;
+
+	}
+
+}
