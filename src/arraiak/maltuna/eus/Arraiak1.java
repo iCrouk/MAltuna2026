80b14def32d95c6765629c5ff6ec3d52bdf1a4b6
iCrouk <ikercoranti@gmail.com>
2026-09-16 18:30:52 +0200

    [UPD] Add 3 more arrays, one int, one String, one char for explane java file for students

--- src/arraiak/maltuna/eus/Arraiak1.java
+++ src/arraiak/maltuna/eus/Arraiak1.java
@@ -14,6 +14,9 @@ public class Arraiak1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nireArraia = new int[10];
+		int[] nireZenbakiak = {5,6,74,2,1,77};
+		String[] nireIzenak = {"Iker","Mikel","Naroa","Izaro"};
+		char [] nireKarakterak = {'a','b','c','d','e','f'};
 
 	}
 
