0562fb5d689dd857e480e064db58885121dbcf68
icoranti <icoranti@localhost>
2026-09-21 13:53:28 +0200

    [UPD] Ariketa5.java re-do

--- src/ariketak1/maltuna/eus/Ariketa5.java
+++ src/ariketak1/maltuna/eus/Ariketa5.java
@@ -30,27 +30,6 @@ public class Ariketa5 {
 		}
 	
 		System.out.println("Bi zenbakien artean:"+handiena+" da handiena");
-
-		//INPUT
-		int znb1;
-		int znb2;
-		int handiena;
-		//EGITEKOAK
-		Scanner sc = new Scanner(System.in);
-		//lehen zenbakia eskatu eta gorde
-		System.out.println("Idatzi lehen zenbakia, mesedez");
-		znb1 = sc.nextInt();
-		//bigarren zenbakia eskatu eta gorde
-		System.out.println("Idatzi bigarren zenbakia, mesedez");
-		znb2 = sc.nextInt();
-		
-		if( znb1 < znb2) {
-			handiena = znb2;
-		} else {
-			handiena = znb1;
-		}
-		//OUTPUT
-		System.out.println ("handiena: "+handiena+" da");
-		sc.close();
+		nireSc.close();
 	}
 }
