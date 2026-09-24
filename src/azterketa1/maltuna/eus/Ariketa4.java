41991b2a9bb1957157ae85f5d9553ccd096a3b79
icoranti <icoranti@localhost>
2026-09-24 09:27:20 +0200

    [UPD] modify the code to adapt do while structure, exam asked to use it.

--- src/azterketa1/maltuna/eus/Ariketa4.java
+++ src/azterketa1/maltuna/eus/Ariketa4.java
@@ -4,10 +4,20 @@ public class Ariketa4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int i=1;
 		int batuketa= 0;
 		int biderketa = 1;
 		
+		do {
+			if (i%7 == 0) {
+				if (i%2 == 0) {
+					batuketa += i;
+					biderketa *= i;
+				}
+			}
+			i++;	
+		}while(i<1000);
+		/*
 		for (int i = 1;i<1000;i++) {
 			if (i%7 == 0) {
 				if (i%2 == 0) {
@@ -16,6 +26,7 @@ public class Ariketa4 {
 				}
 			}
 		}
+		*/
 		System.out.println("Batuketaren emaitza: "+batuketa);
 		System.out.println("Biderketaren emaitza: "+biderketa);
 	}
